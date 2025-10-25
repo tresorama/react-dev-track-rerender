86ea441629aa584df03561cd5c1f433a1c3c8f90
Jacopo Marrone <jacopo.marrone27@gmail.com>
2025-10-25 15:47:51 +0200

    chore: consumer-repo-next - counter - add `<UiCounterIsZeroLabel>`

--- apps/consumer-repo-next/app/(home)/components/counter/react-context/index.tsx
+++ apps/consumer-repo-next/app/(home)/components/counter/react-context/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { CounterProvider, useCounterActions, useCounterState } from "./state";
-import { UiCounterActions, UiCounterCard, UiCounterCount } from "../ui";
+import { UiCounterActions, UiCounterCard, UiCounterCount, UiCounterIsZeroLabel } from "../ui";
 
 
 export function Counter_RC() {
@@ -17,6 +17,7 @@ function TheCounter() {
   return (
     <UiCounterCard>
       <CounterCount />
+      <CounterIsZero />
       <CounterActions />
     </UiCounterCard>
   );
@@ -24,6 +25,10 @@ function TheCounter() {
 function CounterCount() {
   const count = useCounterState().raw.count;
   return <UiCounterCount count={count} />;
+}
+function CounterIsZero() {
+  const isZero = useCounterState().derived.isZero;
+  return <UiCounterIsZeroLabel isZero={isZero} />;
 }
 
 function CounterActions() {
